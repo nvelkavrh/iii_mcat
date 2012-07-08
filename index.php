38c39b6273407eeb044913c5378005a6cc52034b
nvelkavrh <nvelkavrh@gmail.com>
2012-07-07 19:09:00 -0700

    Edited <title> tag.

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html> 
 <html> 
 	<head> 
-	<title>Wardman Library Mobile Catalog Search</title>
+	<title>Wardman Library Mobile Catalog Home</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="http://code.jquery.com/mobile/1.0.1/jquery.mobile-1.0.1.min.css" />
 	<script src="http://code.jquery.com/jquery-1.6.4.min.js"></script>
